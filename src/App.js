fc7a191269aae6d0cd72ddf9582ca8497d3375c4
Aman Nasir <aman.nasir231@gmail.com>
2023-03-05 02:58:33 +0500

    home section added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./pages/navbar/Navbar";
+import Home from "./pages/Home";
 import "./App.css"
 
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Home />
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import Pic from "../Assets/test.png";
 
 const Home = () => {
   return (
-    <div id='Home'>Home</div>
-  )
-}
+    <div id="Home">
+      <div className="homeContent">
+        <p>HELLO!</p>
+        <h3>
+          I'm <span> Aman Nasir</span>
+        </h3>
+        <h4>A MERN Stack Developer</h4>
+        <div className="buttons">
+          <button>HIRE ME</button>
+          <button>MY WORKS</button>
+        </div>
+      </div>
+      <div className="image">
+        <img src={Pic} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
